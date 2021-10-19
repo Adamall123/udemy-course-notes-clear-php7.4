3821ebc6bede8924fe7642227c8267bd515ae30a
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-19 22:42:05 +0200

    add structure files

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php 
 
-function dump($data)
-{
-    echo '<div 
-    style="
-    display: inline-block;
-    padding: 0px 10px;
-    border: 1px solid gray;
-    background: lightgray
-    ">
-    <pre>';
-    print_r($data);
-    echo '</pre></div>';
-}
+// include - includuje plik, w którym mamy jakiś kod, którego chcemy użyć.
+include ("src/Utils/debug.php");
+// include_once - zaimportowany może być tylko jeden raz, gdy zainkludujemy go drugi raz to już to php zignoruje.
 
-dump(['test', 'funkcja', 'debugujaca']);
+// require - gdy nie uda się zaimportować skrypt przerwie swoje działanie , a include po niepoprawnym czytaniu dalej będzie wczytywać skrypt. 
+// require_once - wczyta raz , analogicznie do include_once, gdy zainkludujemy do drugi raz to już php zignoruje. Różnice, że require_once wymaga ten plik, gdyż po nieznalezieniu
+// skrypt przestanie działać.
+
+$test = 'test';
+
+dump($test);
