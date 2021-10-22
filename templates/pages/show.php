dae57de3c3bcf14257cdf13b825d561651736325
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-22 16:47:53 +0200

    add escape method to view class

--- templates/pages/show.php
+++ templates/pages/show.php
@@ -2,10 +2,10 @@
     <?php $note = $params['note'] ?? null; ?>
     <?php if($note):?>
     <ul>
-        <li>Id: <?php echo ($note['id']) ?></li>
-        <li>Tytuł: <?php echo htmlentities(($note['title'])) ?></li>
-        <li><?php echo htmlentities(($note['description'])) ?></li>
-        <li>zapisano: <?php echo htmlentities(($note['created'])) ?></li>
+        <li>Id: <?php echo $note['id'] ?></li>
+        <li>Tytuł: <?php echo $note['title'] ?></li>
+        <li><?php echo $note['description'] ?></li>
+        <li>zapisano: <?php echo $note['created'] ?></li>
     </ul>
     <?php else: ?>
        <div>Brak notatki do wyświetlenia </div> 
