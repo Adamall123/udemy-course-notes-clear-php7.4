7dbe4a480708ef9c40f0a8ba45b61862c5fda867
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-23 22:21:12 +0200

    add autoload method

--- index.php
+++ index.php
@@ -3,29 +3,20 @@
 
 declare(strict_types = 1); //https://stackoverflow.com/questions/48723637/what-do-strict-types-do-in-php
 
-namespace App;
+spl_autoload_register(function(string $classNamespace){
+    $path = str_replace(['\\', 'App/'],['/',''], $classNamespace);
+    $path = "src/$path.php";
+    require_once($path);
+});
 
-use App\Request;
-use App\Exception\AppException;
-use App\Exception\ConfigurationException;
-use Throwable;
-
-// include - includuje plik, w którym mamy jakiś kod, którego chcemy użyć.
-include ("src/Utils/debug.php");
-require_once("src/NoteController.php");
-require_once("src/Request.php");
-
+require_once("src/Utils/debug.php");
 $configuration = require_once("config/config.php");
 
-// include_once - zaimportowany może być tylko jeden raz, gdy zainkludujemy go drugi raz to już to php zignoruje.
-
-// require - gdy nie uda się zaimportować skrypt przerwie swoje działanie , a include po niepoprawnym czytaniu dalej będzie wczytywać skrypt. 
-// require_once - wczyta raz , analogicznie do include_once, gdy zainkludujemy do drugi raz to już php zignoruje. Różnice, że require_once wymaga ten plik, gdyż po nieznalezieniu
-// skrypt przestanie działać.
-
-// $test = 'test';
-
-// dump($test);
+use App\Request;
+use App\Controller\AbstractController;
+use App\Controller\NoteController;
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
 
 $request = new Request($_GET, $_POST);
 
