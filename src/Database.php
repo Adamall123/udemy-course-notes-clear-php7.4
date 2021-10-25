ba6d0fddda39ff3a1f115719c4837eb36ec79417
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-25 16:55:58 +0200

    add sort by title and by date

--- src/Database.php
+++ src/Database.php
@@ -67,10 +67,20 @@ class Database
             throw new StorageException('Nie udało się zedytować notatki.',400);
         }
     }
-    public function getNotes(): array
+    public function getNotes(string $sortBy, string $sortOrder): array
     {
+        
         try{
-            $query = "SELECT id,title ,created FROM notes";
+            if(!in_array($sortBy, ['created', 'title'])){
+                $sortBy = 'title';
+            }
+            if(!in_array($sortOrder, ['asc', 'desc'])){
+                $sortOrder = 'desc';
+            }
+            $query = "SELECT id,title ,created 
+                      FROM notes
+                      ORDER BY $sortBy $sortOrder
+                      ";
             $result = $this->conn->query($query);
             return $result->fetchAll(PDO::FETCH_ASSOC);
             // foreach($result as $row) {
--- templates/pages/list.php
+++ templates/pages/list.php
@@ -31,6 +31,27 @@
                 ?>
             <?php endif; ?>
         </div>
+
+        <?php 
+            $sort = $params['sort'] ?? [];
+            $by = $sort['by'] ?? 'title';
+            $order = $sort['order'] ?? 'desc';
+        ?>
+
+        <div>
+            <form class="settings-form" action="/" method="GET">
+            <div>
+                <div>Sortuj po:</div>
+                <label>tytule: <input name="sortby" type="radio" value="title" <?php echo $by === 'title' ? 'checked': '' ?>></label>
+                <label>dacie: <input name="sortby" type="radio" value="created" <?php echo $by === 'created' ? 'checked': '' ?>></label>
+                <div>Kierunek sortowania</div>
+                <label>rosnąco: <input name="sortorder" type="radio" value="asc" <?php echo $order === 'asc' ? 'checked': '' ?>></label>
+                <label>malejąco: <input name="sortorder" type="radio" value="desc"  <?php echo $order === 'desc' ? 'checked': '' ?> ></label>
+            </div>
+                <input type="submit" value="Wyślij"/>
+            </form>
+        </div>
+
         <div class="tbl-header">
             <table cellpadding="0" cellspacing="0" border="0">
                 <thead>
